409d0578ff1c26b560cb42994bd88699c4661cd0
Junior2k5 <eplj@uol.com.br>
2021-07-21 15:32:56 -0300

    Mais um commit
    Commit 2

--- django1/settings.py
+++ django1/settings.py
@@ -112,7 +112,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/static/' #pra usar durante o desenvolvimento apenas
+STATIC_ROOT = os.path(BASE_DIR, 'staticfiles') #usado durante a pridução
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
