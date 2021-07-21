9fe666feafb5b1a8352ac0227d51af397a779cac
Junior2k5 <eplj@uol.com.br>
2021-07-21 14:33:44 -0300

    Commit inicial
    First commit

---
+++ core/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+
+from .models import Produto
+
+# Create your views here.
+def index(request):
+    produtos = Produto.objects.all()
+    context = {
+        'curso': 'Programação Web com Django Framework',
+        'outros': 'Django é massa!',
+        'produtos': produtos
+    }
+    return render(request, 'index.html', context)
+
+def contato(request):
+    return render(request, 'contato.html')
+
+def produto(request, pk):
+    prod = Produto.objects.get(id=pk)
+    context = {
+        'produto': prod
+    }
+    return render(request, 'produto.html', context)
